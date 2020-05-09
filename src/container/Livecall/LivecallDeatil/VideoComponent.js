a964f181bc7a685297242e00a4dfc26543cc3e4d
zhoujuqi <zhoujuqi5638@100.me>
2020-05-09 18:08:30 +0800

    iframe

--- src/container/Livecall/LivecallDeatil/VideoComponent.js
+++ src/container/Livecall/LivecallDeatil/VideoComponent.js
@@ -34,27 +34,25 @@ class VideoComponent extends Component {
 
   initVideo = props => {
     const { equipmentIp, equipmentPort } = props.data
-    try {
-      this.videoView = new mainClass()
-      const rst = this.videoView.fasterLogin(
-        equipmentIp,
-        equipmentPort,
-        'admin',
-        '123456'
-      )
-      if (rst.code !== 1) {
-        message.warning(rst.msg)
-      }
-    } catch (e) {
-      message.error('监控初始化失败')
-    }
+    // try {
+    //   this.videoView = new mainClass()
+    //   const rst = this.videoView.fasterLogin(
+    //     equipmentIp,
+    //     equipmentPort,
+    //     'admin',
+    //     '123456'
+    //   )
+    //   if (rst.code !== 1) {
+    //     message.warning(rst.msg)
+    //   }
+    // } catch (e) {
+    //   message.error('监控初始化失败')
+    // }
   }
 
   render() {
     return (
-        <div className="ocxStyle">
-            <div id="playerContainer" />
-        </div>
+      <iframe src='//47.106.164.38/plugins/video/video.html' height='100%' width='100%'></iframe>
     )
   }
 }
